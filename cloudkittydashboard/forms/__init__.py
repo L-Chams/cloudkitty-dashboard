6dbf315e1f04ceae3f74a2db3be44b27b198dbc3
Leonie <leonie@stackhpc.com>
2025-08-13 14:00:36 +0000

    Added forms folder to CK dashboard
    Change-Id: I3e708cb7e69778a4632b19cb355c7f595caaf21a
    Signed-off-by: Leonie <leonie@stackhpc.com>

---
+++ cloudkittydashboard/forms/__init__.py
@@ -0,0 +1,37 @@
+# Copyright 2012 Nebula, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# Importing non-modules that are not used explicitly
+
+# FIXME(gabriel): Legacy imports for API compatibility.
+from django.forms.fields import BooleanField
+from django.forms.fields import DateField
+from django.forms.forms import Form
+
+
+# Convenience imports for public API components.
+from cloudkittydashboard.forms.base import CheckBoxForm
+from cloudkittydashboard.forms.base import DateForm
+
+
+__all__ = [
+    "DateForm",
+    "CheckBoxForm",
+
+    # From django.forms.fields
+    'DateField', 'BooleanField',
+
+    # From django.forms.forms
+    'Form',
+]
